ff6689a3fc69b166d17bbd73fd29e4a03b0df067
sofiebirch <145270791+sofiebirch@users.noreply.github.com>
2023-09-23 14:18:42 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,6 +3,6 @@ const button = document.querySelector("button");
 button.addEventListener("click", updateName);
 
 function updateName() {
-  const text = prompt("Skriv noget tekst!");
+  const text = prompt("Skriv noget tekst her!");
   button.textContent = `Player 1: ${text}`;
 }
