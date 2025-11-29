9e2d009f02720c9f62213fa4b1a888a3f8a05466
sofiebirch <145270791+sofiebirch@users.noreply.github.com>
2025-11-29 18:12:54 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
 const button = document.querySelector("button");
 
-button.addEventListener("click", updateName);
-
 button.addEventListener("click", function () {
-  document.body.style.backgroundColor = "lavender";
+  const currentColor = document.body.style.backgroundColor;
+
+  if (currentColor === "lavender") {
+    document.body.style.backgroundColor = "white";
+  } else {
+    document.body.style.backgroundColor = "lavender";
+  }
 });
