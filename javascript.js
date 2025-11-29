d89a8a7e528bb67b05889f64e76a16728c419ccb
sofiebirch <145270791+sofiebirch@users.noreply.github.com>
2025-11-29 17:43:00 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,6 +2,10 @@ const button = document.querySelector("button");
 
 button.addEventListener("click", updateName);
 
+button.addEventListener("click", function () {
+  document.body.style.backgroundColor = "lavender";
+});
+
 function updateName() {
   const text = prompt("Skriv noget tekst her!");
   button.textContent = `${text}`;
