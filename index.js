a6be437475a3d383e85a65cbabbb583ef278b546
sofiebirch <145270791+sofiebirch@users.noreply.github.com>
2025-12-18 13:25:52 +0100

    Update and rename javascript.js to index.js

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-fetch("http://localhost:4567/books")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    });
-
 const button = document.querySelector("button");
 
 button.addEventListener("click", function () {
