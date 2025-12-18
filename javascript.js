092e1b34380a47155eba62b0e61aee532db33139
sofiebirch <145270791+sofiebirch@users.noreply.github.com>
2025-12-18 13:16:56 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
+fetch("http://localhost:4567/books")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });
+
 const button = document.querySelector("button");
 
 button.addEventListener("click", function () {
